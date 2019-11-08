933600e10b0d58b6a504c673e1975be202da685f
mathdavy <matheusuzumaki26@gmail.com>
2019-11-08 01:15:49 -0300

    Model produto

--- src/Model/Pedido.java
+++ src/Model/Pedido.java
@@ -3,26 +3,31 @@ import State.State.Status;
 import java.util.Date;
 
 public class Pedido {
-	
-	double preco;
+	private int ID;
+	private double preco;
 	private Status status;
 	private Date data;
 
 	public double getPreco() {
 		return preco;
 	}
+	
 	public void setPreco(double preco) {
 		this.preco = preco;
 	}
+	
 	public Status getStatus() {
 		return status;
 	}
+	
 	public void setStatus(Status status) {
 		this.status = status;
 	}
+	
 	public Date getData() {
 		return data;
 	}
+	
 	public void setData(Date data) {
 		this.data = data;
 	}
---
+++ src/Model/Produto.java
@@ -0,0 +1,42 @@
+package Model;
+
+public class Produto {
+	private int ID;
+	private String nome;
+	private String descricao;
+	private double preco;
+	
+	public Produto(int ID, String nome, String descricao, double preco) {
+        this.ID = ID;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+}

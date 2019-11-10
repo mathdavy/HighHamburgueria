28fbae8bfd35ce2d5bc5c9e45f211faa9c6f339b
mathdavy <matheusuzumaki26@gmail.com>
2019-11-10 15:48:27 -0300

    main

--- src/ch/Controller/LoginController.java
+++ src/ch/Controller/LoginController.java
@@ -1,15 +1,53 @@
 package ch.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController implements Initializable{
-
+	
+	@FXML
+	private Button buttonLogin;
+	@FXML
+	private TextField textFieldUsuario;
+	@FXML
+	private TextField textFieldSenha;
+	
+	@FXML
+	public void Login(ActionEvent event) throws IOException {
+		/*try{
+			if(textFieldUsuario.getText().equals("user") && textFieldSenha.getText().equals("pass")) {
+			
+				Stage stage = new Stage();
+				
+				Parent root = FXMLLoader.load(getClass().getResource("View/MenuFuncionario.fxml"));
+				
+				Scene scene = new Scene(root);
+				stage.setScene(scene);
+				stage.setTitle("High Hamburgueria");
+				stage.setResizable(false);
+				stage.show();
+			}else {
+				
+			}
+		}catch(Exception e) {
+			
+		}*/
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
---
+++ src/ch/Controller/MenuFuncionarioController.java
@@ -0,0 +1,28 @@
+package ch.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class MenuFuncionarioController implements Initializable{
+	
+	@FXML
+	private Button buttonCadastrarComanda;
+	@FXML
+	private Button buttonFazerPedido;
+	@FXML
+	private Button buttonAlterarPedido;
+	@FXML
+	private Button buttonFinalizarComanda;
+	@FXML
+	private Button buttonHistoricoPedidos;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+}
